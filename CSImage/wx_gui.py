63861b95fab26441ec2e39e079bdf4639c63bb0f
Chromebook <sam@woodward.fyi>
2022-03-08 22:29:28 -0500

    Add .SetSizer() to the `Panel` for the Button to be clickable.

--- CSImage/wx_gui.py
+++ CSImage/wx_gui.py
@@ -4,18 +4,59 @@ from search import process
 
 class MainWindow(wx.Frame):
 
-    def __init__(self, parent, title):
-        super().__init__(parent, title=title, size=(300, 175))
+    def __init__(self, parent):
+        super().__init__(parent, size=(600, 325))
         self.Show(True)
-
-        # Status bar
+        self.select_panel = None
+        self.process_panel = None
+        self.setup_select_panel()
         self.status_bar = self.CreateStatusBar()
         self.status_bar.PushStatusText('Select a starting point to begin...')
 
-        cwd = self.get_cwd()
-        self.run(cwd)
+    def setup_select_panel(self):
+        if self.select_panel is None:
+            self.select_panel = wx.Panel(self)
+            label = wx.StaticText(
+                self.select_panel,
+                label='Choose a starting path below'
+            )
+            start_btn = wx.Button(self.select_panel, label='Select')
+            start_btn.Bind(wx.EVT_BUTTON, self.get_cwd, start_btn)
+            sizer = wx.BoxSizer(wx.VERTICAL)
+            sizer.AddStretchSpacer(1)
+            sizer.Add(label, 0,  wx.ALIGN_CENTER, 20)
+            sizer.AddSpacer(25)
+            sizer.Add(start_btn, 0,  wx.ALIGN_CENTER, 20)
+            sizer.AddStretchSpacer(1)
+            sizer.SetSizeHints(self.select_panel)
+            self.select_panel.SetSizer(sizer)
+        self.select_panel.Show()
 
-    def get_cwd(self):
+    def _setup_select_panel(self):
+        if self.select_panel is None:
+            self.select_panel = wx.Panel(self)
+            label = wx.StaticText(
+                self.select_panel,
+                label='Choose a starting path below'
+            )
+            start_btn = wx.Button(self, label='Select')
+            self.Bind(wx.EVT_BUTTON, self.get_cwd, start_btn)
+            # sizer is broken when on subsequent runs
+            sizer = wx.BoxSizer(wx.VERTICAL)
+            sizer.AddStretchSpacer(1)
+            sizer.Add(label, 0,  wx.ALIGN_CENTER, 20)
+            sizer.AddSpacer(25)
+            sizer.Add(start_btn, 0,  wx.ALIGN_CENTER, 20)
+            sizer.AddStretchSpacer(1)
+            sizer.SetSizeHints(self.select_panel)
+            self.SetSizer(sizer)
+        self.select_panel.Show()
+
+    def teardown_select_panel(self):
+        self.select_panel.Destroy()
+        self.select_panel = None
+
+    def get_cwd(self, *event_args, **event_kwargs):
         cwd_dialog = wx.DirDialog(
             None,
             'Choose staring location',
@@ -25,37 +66,40 @@ class MainWindow(wx.Frame):
         if cwd_dialog.ShowModal() != wx.ID_OK:
             err_dialog = wx.MessageDialog(
                 cwd_dialog,
-                caption=f'You must select a location to continue.',
-                style=wx.ERROR | wx.OK | wx.CENTRE
+                'You must select a location to continue.',
+                caption='Error: Directory Not Selected',
+                style=wx.ICON_ERROR | wx.OK | wx.CENTRE
             )
             err_dialog.ShowModal()
-            del cwd_dialog
-            return self.get_cwd()
+            return
         cwd = cwd_dialog.GetPath()
         if not os.path.isdir(cwd):
             err_dialog = wx.MessageDialog(
                 cwd_dialog,
-                caption=f'{cwd} does not exist',
-                style=wx.ERROR | wx.OK | wx.CENTRE
+                f'{cwd} does not exist',
+                style=wx.ICON_ERROR | wx.OK | wx.CENTRE
             )
             err_dialog.ShowModal()
         else:
-            return cwd
+            self.run(cwd)
+            return
 
     def run(self, cwd):
         # 0: Processing...{fname}
         # 1: Total Processed
         # 2: Matches found
+        self.teardown_select_panel()
+        if self.process_panel is None:
+            self.process_panel = wx.Panel(self, wx.ID_ANY)
         processed, matches, prev_processed, prev_matches = 0, 0, 0, 0
-        self.status_bar.SetFieldsCount(number=3, widths=(-1, 100, 100))
+        self.status_bar.SetFieldsCount(number=3, widths=(-1, 100, 100))  # check min width
         # use self.status_bar.SetStatusText(text, i=int)
         self.status_bar.SetStatusText(f'Processing {cwd}...', i=0)
         self.status_bar.SetStatusText(f'Total: {processed:,}', i=1)
         self.status_bar.SetStatusText(f'Matches: {matches:,}', i=2)
-        pass
 
 
 if __name__ == '__main__':
     app = wx.App(False)
-    frame = MainWindow(None, 'CSImage Searver - wxPython')
+    frame = MainWindow(None)
     app.MainLoop()
