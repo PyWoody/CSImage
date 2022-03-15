e9e1ee1aefc78a430308913ffba40a06655234b6
Chromebook <sam@woodward.fyi>
2022-03-14 22:00:44 -0400

    Converted main_window to QStackedWidget. Layout sizing is not expanding to parent. Images are not appearing. Thread is slowing main GUI.

--- CSImage/pyside.py
+++ CSImage/pyside.py
@@ -1,13 +1,17 @@
+import os
 import sys
 import time
 import zlib
 
 from PySide6 import QtCore
 from PySide6.QtGui import QImage, QPixmap
-from PySide6.QtWidgets import QGridLayout, \
+from PySide6.QtWidgets import QErrorMessage, \
+                              QFileDialog, \
+                              QGridLayout, \
                               QLabel, \
                               QMainWindow, \
                               QPushButton, \
+                              QStackedWidget, \
                               QVBoxLayout, \
                               QWidget
 from search import ImageQueue, process
@@ -17,37 +21,36 @@ class MainWindow(QMainWindow):
 
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.resize(600, 250)
+        self.resize(600, 350)
         self.carousel_widget = None
         self.results_widget = None
         self.select_widget = None
-        self.main_window = QMainWindow(self)
-        self.processed, self.matches = 0, 0
+        self.main_window = QStackedWidget(self)
         self.status_bar = self.statusBar()
         self.status_msg = QLabel(f'Processed: {0} | Matches: {0}')
         self.status_bar.addPermanentWidget(self.status_msg)
         self.setup_select_widget()
         self.setup_carousel_widget()
         self.setup_results_widget()
-        # self.central_widget = self.select_widget
-        self.central_widget = self.carousel_widget
-        self.central_widget.show()
-        '''
-        self.central_widget.setAlignment(
-            QtCore.Qt.AlignHCenter | QtCore.Qt.AlignVCenter
-        )
-        '''
-        self.main_window.setCentralWidget(self.central_widget)
+        self.main_window.addWidget(self.select_widget)
+        self.main_window.addWidget(self.carousel_widget)
+        self.main_window.addWidget(self.results_widget)
+        self.main_window.setCurrentWidget(self.select_widget)
+        layout = QVBoxLayout()
+        layout.addWidget(self.main_window)
+        self.setLayout(layout)
         self.main_window.show()
         self.image_carousel = ImageQueue()
-        self.run(r'/home/sam/Projects')
 
     def run(self, cwd):
+        self.main_window.setCurrentWidget(self.carousel_widget)
         processed, matches = 0, 0
         self.update_progress_status(processed=processed, matches=matches)
-        carousel_thread = Thread(target=self.spin_the_carousel, daemon=True)
+        img_label = QLabel(self.main_window)
+        carousel_thread = Thread(target=self.spin_the_carousel, args=(img_label,), daemon=True)
         carousel_thread.start()
         for is_match, fpath, mem in process(cwd):
+            print(fpath)
             if is_match:
                 matches += 1
             processed += 1
@@ -58,16 +61,15 @@ class MainWindow(QMainWindow):
         carousel_thread.join()
         # self.show_results(cwd, processed, matches)
 
-    def spin_the_carousel(self):
+    def spin_the_carousel(self, img_label):
         for result in self.image_carousel:
             is_match, fpath, mem = result
             # image = QImage.loadFromData(QtCore.QByteArray(mem))
             image = QImage()
             if image.loadFromData(zlib.decompress(mem)):
-                test = QLabel()
-                test.setPixmap(QPixmap.fromImage(image))
-                test.show()
-                print(fpath)
+                img_label.setPixmap(QPixmap.fromImage(image))
+                # print(fpath)
+                # time.sleep(10)
 
     def setup_select_widget(self):
         label = QLabel('Choose a starting path below')
@@ -76,8 +78,7 @@ class MainWindow(QMainWindow):
         layout = QVBoxLayout()
         layout.addWidget(label)
         layout.addWidget(select_btn)
-        # self.select_widget = QWidget()
-        self.select_widget = QLabel('test')
+        self.select_widget = QWidget()
         self.select_widget.setLayout(layout)
 
     def setup_carousel_widget(self):
@@ -93,24 +94,27 @@ class MainWindow(QMainWindow):
 
     def setup_results_widget(self):
         if self.results_widget is None:
-            self.results_widget = QWidget(self)
+            self.results_widget = QWidget()
 
     @QtCore.Slot()
     def get_cwd(self):
         cwd_dialog = QFileDialog(self)
         cwd_dialog.setFileMode(QFileDialog.Directory)
         if cwd_dialog.exec():
-            cwd = cwd_dialog.selectedFiles()
-            if not os.path.isdir(cwd):
-                error_dialog = QErrorMessage(self)
-                error_dialog.showMessage('You must select a location to continue.')
-                return self.get_cwd()
-            else:
-                self.run(cwd)
-                return
+            if cwd := cwd_dialog.selectedFiles():
+                cwd = cwd[0]
+                if not os.path.isdir(cwd):
+                    error_dialog = QErrorMessage(self)
+                    error_dialog.showMessage('You must select a location to continue.')
+                    # error_dialog.exe theses
+                    return self.get_cwd()
+                else:
+                    self.run(cwd)
+                    return
         else:
             error_dialog = QErrorMessage(self)
             error_dialog.showMessage('You must select a location to continue.')
+            # error_dialog.exe theses
             return self.get_cwd()
 
     def update_progress_status(self, *, processed, matches):
